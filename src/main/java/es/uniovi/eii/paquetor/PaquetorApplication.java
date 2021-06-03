4f03df2b1a448837d0b68e74e4651f1ebc2553d7
Ramón Vila <ramon.vilafer@gmail.com>
2021-06-03 11:05:00 +0200

    Created basic package strucutre and added datasource configuration

---
+++ src/main/java/es/uniovi/eii/paquetor/PaquetorApplication.java
@@ -0,0 +1,13 @@
+package es.uniovi.eii.paquetor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaquetorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaquetorApplication.class, args);
+    }
+
+}
