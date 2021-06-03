0c4582b38fad1c32cecd8070848e2833866aa4f9
Ramón Vila <ramon.vilafer@gmail.com>
2021-06-03 14:12:06 +0200

    Added UsersService with some basic methods. Created the base Controller

--- src/main/java/es/uniovi/eii/paquetor/controllers/UsersController.java
+++ src/main/java/es/uniovi/eii/paquetor/controllers/UsersController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UsersController {
-    
+
     @Autowired
     UsersService usersService;
 
--- src/main/java/es/uniovi/eii/paquetor/services/UsersService.java
+++ src/main/java/es/uniovi/eii/paquetor/services/UsersService.java
@@ -1,11 +1,45 @@
 package es.uniovi.eii.paquetor.services;
 
+import es.uniovi.eii.paquetor.entities.User;
 import es.uniovi.eii.paquetor.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsersService {
 
     @Autowired
     UsersRepository usersRepository;
 
+    @PostConstruct
+    public void init() { /**/ }
+
+    public List<User> getUsers() {
+        List<User> users = new ArrayList<User>();
+        usersRepository.findAll().forEach(users::add);
+        return users;
+    }
+
+    public User getUser(Long id) {
+        return usersRepository.findById(id).get();
+    }
+
+    public void addUser(User user) {
+        // user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        usersRepository.save(user);
+    }
+
+    public User getUserByEmail(String email) {
+        return usersRepository.findByEmail(email);
+    }
+
+    public void deleteUser(Long id) {
+        usersRepository.deleteById(id);
+    }
+
+    public void editUser(User user) {
+        usersRepository.save(user);
+    }
 }
